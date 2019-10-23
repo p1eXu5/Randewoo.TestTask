877c5d836a9d4148bc5e2899c783324d3a31d0a1
First Second <v1adp1exu5@gmail.com>
2019-10-23 13:41:22 +0300

    chore: add start up logic

--- src/Randewoo.TestTask.DesktopClient/App.xaml.cs
+++ src/Randewoo.TestTask.DesktopClient/App.xaml.cs
@@ -2,9 +2,13 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using Microsoft.EntityFrameworkCore;
+using Randewoo.TestTask.DataContext;
+using Randewoo.TestTask.DesktopClient.ViewModels;
 
 namespace Randewoo.TestTask.DesktopClient
 {
@@ -13,5 +17,22 @@ namespace Randewoo.TestTask.DesktopClient
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup( StartupEventArgs e )
+        {
+            var connectionString = ConfigurationManager.ConnectionStrings[ "TestDb" ].ConnectionString;
+
+            if ( !String.IsNullOrWhiteSpace( connectionString ) ) 
+            {
+                var options = new DbContextOptionsBuilder< TestDbContext >( ).UseSqlServer( connectionString ).Options;
+
+                var dbContext = new TestDbContext( options );
+                var mvm = new MainViewModel( dbContext );
+                var mainWindow = new MainWindow {
+                    DataContext = mvm,
+                };
+
+                mainWindow.Show();
+            }
+        }
     }
 }
---
+++ src/Randewoo.TestTask.DesktopClient/ViewModels/MainViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Agbm.Wpf.MvvmBaseLibrary;
+using Randewoo.TestTask.DataContext;
+
+namespace Randewoo.TestTask.DesktopClient.ViewModels
+{
+    public class MainViewModel : ViewModel
+    {
+        private readonly TestDbContext _dbContext;
+        public MainViewModel( TestDbContext dbContext )
+        {
+            _dbContext = dbContext;
+        }
+
+
+    }
+}
